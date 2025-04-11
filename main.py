8a2e945458725c0679399840a9a97b824188ab44
Zayan <zayan.qazi.mail@gmail.com>
2025-04-10 22:27:47 -0500

    removed whitespaces from system prompt. Novel outliner works!!

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from load_env import load_env_vars
 import argparse
 from utilities.io import load_text
 from config import Config
+from langchain_core.prompts import ChatPromptTemplate
 
 if __name__ == "__main__":
     # load environment variables
@@ -19,10 +20,29 @@ if __name__ == "__main__":
     args = parser.parse_args()
     provider, model = args.provider, args.model 
     llm = get_llm(provider, model)
-   
-    # Produce novel outline
-    prompt = load_text(Config.plot_generator_prompt)
+  
+    # Novel metadata (genre, tone, etc)
+    config = Config()
+    novel_metadata: dict = config.get_novel_metadata()
     breakpoint()
-    response = llm.invoke(prompt).content
+    
+    # Get user input
+    user_input = input("Describe the novel you want to write:\n")
+
+    # Produce novel outline
+    outliner_prompt_raw_text = load_text(config.plot_generator_prompt)
+    outliner_prompt = ChatPromptTemplate.from_messages([
+        ("system", outliner_prompt_raw_text),
+        ("user", "{user_input}")]).partial(
+        genre=novel_metadata['genre'],
+        tone=novel_metadata['tone'],
+        main_character=novel_metadata['main_character'],
+        themes=novel_metadata['themes'],
+        authors_message=novel_metadata['authors_message']
+    )
+    
+    chain = outliner_prompt | llm
+    breakpoint()
+    response = chain.invoke({"user_input" : user_input}).content
     print(response)
     # outline = generate_novel_outline() 
