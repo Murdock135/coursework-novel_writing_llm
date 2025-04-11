49a3c4f098b90b2058b8ae22dba675ba17ccbbaf
Zayan <zayan.qazi.mail@gmail.com>
2025-04-10 21:32:29 -0500

    not using staticmethod in config.py

--- config.py
+++ config.py
@@ -1,39 +1,37 @@
 import os
 
 class Config:
-    self.project_dir = os.path.dirname(__file__)
-    self.path_to_prompts = os.path.join(os.path.abspath(project_dir), 'sys_messages')
-    self.plot_generator_prompt = os.path.join(path_to_prompts, 'plot_generator.txt')
+    project_dir = os.path.dirname(__file__)
+    path_to_prompts = os.path.join(os.path.abspath(project_dir), 'sys_messages')
+    plot_generator_prompt = os.path.join(path_to_prompts, 'plot_generator.txt')
     
     # novel metadata
-    self.genre = "historical fiction"
-    self.tone = "gloomy and analytical"
-    self.main_character = "May"
-    self.themes = ["betrayal", "existential crises", "illusion of the light at the end of the tunnel"]
-    self.authors_message = None
+    genre = "historical fiction"
+    tone = "gloomy and analytical"
+    main_character = "May"
+    themes = ["betrayal", "existential crises", "illusion of the light at the end of the tunnel"]
+    authors_message = None
 
     # output paths
-    self.plot_outline_path = 'data/plot_outline.toml'
-    self.scenes_path = 'data/scene_summaries'
-    self.character_sheet = 'data/character_sheet'
+    plot_outline_path = 'data/plot_outline.toml'
+    scenes_path = 'data/scene_summaries'
+    character_sheet_path = 'data/character_sheet.txt'
 
     # interaction settings
-    self.use_interactive_decisions = False
+    use_interactive_decisions = False
 
-    @staticmethod
-    def get_novel_metadata():
+    def get_novel_metadata(self):
         return {
-            "genre": Config.genre,
-            "tone": Config.tone,
-            "main_character": Config.main_character,
-            "themes": Config.themes,
-            "authors_message": Config.authors_message
+            "genre": self.genre,
+            "tone": self.tone,
+            "main_character": self.main_character,
+            "themes": self.themes,
+            "authors_message": self.authors_message
         }
 
-    @staticmethod
-    def get_output_paths():
+    def get_output_paths(self):
         return {
-            "plot_outline_path": Config.plot_outline_path,
-            "scenes_path": Config.scenes_path,
-            "character_sheet": Config.character_sheet
+            "plot_outline_path": self.plot_outline_path,
+            "scenes_path": self.scenes_path,
+            "character_sheet_path": self.character_sheet_path
         }
